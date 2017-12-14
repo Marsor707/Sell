51eb4241a6ce35245e2de455551f5750745d1800
marsor <369135912@qq.com>
2017-12-14 15:54:05 +0800

    卖家商品类目修改

---
+++ src/main/java/com/github/marsor707/form/CategoryForm.java
@@ -0,0 +1,18 @@
+package com.github.marsor707.form;
+
+import lombok.Data;
+
+/**
+ * Author: Marsor
+ * Github: https://github.com/Marsor707
+ * Email: 369135912@qq.com
+ */
+@Data
+public class CategoryForm {
+
+    private Integer categoryId;
+
+    private String categoryName;
+
+    private Integer categoryType;
+}
